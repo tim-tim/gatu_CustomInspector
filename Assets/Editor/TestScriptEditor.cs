66833bd67d5f1757563dc0da080d8db2fff2a274
Yum <yunisio@gmail.com>
2016-10-06 17:53:58 +0300

    Initial

---
+++ Assets/Editor/TestScriptEditor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class TestScriptEditor : Editor
+{
+
+
+}
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour
+{
+    #region Public Variables
+    #endregion
+
+    #region Private Variables
+    #endregion
+
+    #region Main Methods
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    #endregion
+}
