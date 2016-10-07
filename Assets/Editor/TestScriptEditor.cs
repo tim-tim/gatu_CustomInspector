77aa58c7d2b06a7fa5dc29ff0faf6213582c3b1b
Yum <yunisio@gmail.com>
2016-10-07 10:11:01 +0300

    start_video_01

--- Assets/Editor/TestScriptEditor.cs
+++ Assets/Editor/TestScriptEditor.cs
@@ -2,8 +2,23 @@
 using UnityEngine;
 using System.Collections;
 
+[CustomEditor(typeof(TestScript))]
 public class TestScriptEditor : Editor
 {
+    #region Public Variables
+    public TestScript targetScript;    
+    #endregion
 
+    #region Private Variables
+    #endregion
 
+    #region Main Methods
+    void OnEnable()
+    {
+
+    }
+    #endregion
+
+    #region Utility Methods
+    #endregion
 }
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -3,22 +3,12 @@ using System.Collections;
 
 public class TestScript : MonoBehaviour
 {
-    #region Public Variables
+    #region Public Variables    
     #endregion
 
     #region Private Variables
     #endregion
 
     #region Main Methods
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-
-    }
-
     #endregion
 }
